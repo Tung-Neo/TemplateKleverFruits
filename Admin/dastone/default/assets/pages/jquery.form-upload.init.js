e3581d6ace61fed5e5287411ebb4e45b4a865feb
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2021-10-31 18:35:10 +0700

    fix-create-product

--- Admin/dastone/default/assets/pages/jquery.form-upload.init.js
+++ Admin/dastone/default/assets/pages/jquery.form-upload.init.js
@@ -1,1 +1,15 @@
-$(function(){$(".dropify").dropify(),$(".dropify-fr").dropify({messages:{default:"Glissez-déposez un fichier ici ou cliquez",replace:"Glissez-déposez un fichier ou cliquez pour remplacer",remove:"Supprimer",error:"Désolé, le fichier trop volumineux"}});var e=$("#input-file-events").dropify();e.on("dropify.beforeClear",function(e,r){return confirm('Do you really want to delete "'+r.file.name+'" ?')}),e.on("dropify.afterClear",function(e,r){alert("File deleted")}),e.on("dropify.errors",function(e,r){console.log("Has Errors")});var r=$("#input-file-to-destroy").dropify();r=r.data("dropify"),$("#toggleDropify").on("click",function(e){e.preventDefault(),r.isDropified()?r.destroy():r.init()})});
+$(function(){
+  $(".dropify").dropify(),
+  $(".dropify-fr").dropify({
+    messages:{
+      default:"Kéo và thả tệp vào đây hoặc nhấp vào",
+      replace:"Kéo và thả tệp hoặc nhấp để thay thế",
+      remove:"Xóa",
+      error:"Xin lỗi, tệp quá lớn"}});
+  var e=$("#input-file-events").dropify();
+  e.on(
+    "dropify.beforeClear",
+    function(e,r){return confirm('Bạn có chắc chắn muốn xóa "'+r.file.name+'" ?')}),
+  e.on("dropify.afterClear",function(e,r){alert("Tệp đã xóa")}),
+  e.on("dropify.errors",function(e,r){console.log("Có lỗi")});
+  var r=$("#input-file-to-destroy").dropify();r=r.data("dropify"),$("#toggleDropify").on("click",function(e){e.preventDefault(),r.isDropified()?r.destroy():r.init()})});
