39e206557dbbe5125704571e134176aaea01b797
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2021-11-05 20:40:53 +0700

    upload-profile

--- KleverFruits/js/myjs.js
+++ KleverFruits/js/myjs.js
@@ -1,3 +1,19 @@
 function hamDropdown() {
  document.querySelector(".noidung_dropdown").classList.toggle("hienThi");
 }
+
+let showPassword = true
+const ipnElement = document.querySelector('#ipnPassword')
+const btnElement = document.querySelector('#btnPassword')
+
+btnElement.addEventListener('click', togglePassword)
+
+function togglePassword() {
+  if (showPassword) {
+    ipnElement.setAttribute('type', 'text')
+    showPassword = false
+  } else {
+    ipnElement.setAttribute('type', 'password')
+    showPassword = true
+  }
+}
